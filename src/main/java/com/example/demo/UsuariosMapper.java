840635db96a141b5b333b1d2be616f3a7fef1e1a
diego <diegoleador333@gmail.com>
2026-01-18 12:07:54 +0100

    arreglo errores

--- src/main/java/com/example/demo/UsuariosMapper.java
+++ src/main/java/com/example/demo/UsuariosMapper.java
@@ -2,16 +2,12 @@ package com.example.demo;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
 
-public class UsuariosMapper {
+public class UsuariosMapper implements RowMapper<Usuarios> {
+	@Override
 	public Usuarios mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return new Usuarios(
-            rs.getLong("id"),
-            rs.getString("nombre"),
-            rs.getString("apellido"),
-            rs.getString("dni"),
-            rs.getString("correo"),
-            rs.getString("password")
-        );
-    }
+		return new Usuarios(rs.getLong("id"), rs.getString("nombre"), rs.getString("apellido"), rs.getString("dni"),
+				rs.getString("correo"), rs.getString("password"));
+	}
 }

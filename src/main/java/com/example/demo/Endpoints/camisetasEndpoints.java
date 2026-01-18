2716fd94b9b6dab44635135e7d55b52f289150f5
Pablo Sainero Valle <pablo.sainero@live.u-tad.com>
2026-01-18 13:57:02 +0100

    Merge branch 'master' of https://github.com/diegoleador333-bit/ProyectoUD5_Pablo_Diego_Nico_Fernando.git

--- src/main/java/com/example/demo/Endpoints/camisetasEndpoints.java
+++ src/main/java/com/example/demo/Endpoints/camisetasEndpoints.java
@@ -33,12 +33,12 @@ public class camisetasEndpoints {
 		return misCamisetas;
 	}
 
-	@GetMapping("/todas")
+	@GetMapping("/{unId}")
 	public List<Camisetas> mostrarCamisetasById(@PathVariable int unId, HttpSession session) {
+		Usuarios usuario = (Usuarios) session.getAttribute("usuario");
 		String sql = "SELECT c.id, c.equipo, c.imagen, c.precio, c.temporada, c.liga, c.nombreDorsal, c.numeroDorsal, c.parche, s.stockS, s.stockM, s.stockL, s.stockXL"
 				+ "FROM Camisetas c" + "INNER JOIN StockPorTalla s ON c.id = s.camiseta_Id"
 				+ "WHERE c.id = :idCamisetaSeleccionada";
-		Usuarios usuario = (Usuarios) session.getAttribute("usuario");
 		List<Camisetas> misCamisetas = jdbcTemplate.query(sql, new CamisetasMapper());
 
 		return misCamisetas;

219b1e750e830e5f65337ef188b0c8c439784785
Fernando García <garcia.fernandomg@gmail.com>
2026-01-18 13:10:09 +0100

    Commit Mejoras identificacion

--- src/main/java/com/example/demo/Identificacion.java
+++ src/main/java/com/example/demo/Identificacion.java
@@ -3,10 +3,13 @@ package com.example.demo;
 import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/identificacion")
@@ -19,7 +22,7 @@ public class Identificacion {
 	}
 
 	@PostMapping("/registro")
-	public String registro(@RequestBody Usuarios nuevo) {
+	public String registro(@RequestBody Usuarios nuevo, HttpSession session) {
 
 		List<Usuarios> existentes = jdbcTemplate.query("SELECT * FROM usuarios WHERE correo = ?", new UsuariosMapper(),
 				nuevo.getCorreo());
@@ -37,12 +40,12 @@ public class Identificacion {
 			    nuevo.getPassword()
 			);
 
-
+		session.setAttribute("usuario", nuevo);
 		return "Usuario registrado correctamente";
 	}
 
 	@PostMapping("/login")
-	public String login(@RequestBody Usuarios login) {
+	public String login(@RequestBody Usuarios login, HttpSession session) {
 
 		List<Usuarios> encontrados = jdbcTemplate.query("SELECT * FROM usuarios WHERE correo = ? AND password = ?",
 				new UsuariosMapper(), login.getCorreo(), login.getPassword());
@@ -52,6 +55,27 @@ public class Identificacion {
 		}
 
 		Usuarios u = encontrados.get(0); 
+		session.setAttribute("usuario", u);
+		
 		return "Login correcto";
 	}
+	
+	@PostMapping("/cerrar")
+	public String logout(HttpSession session) {
+	    session.invalidate();
+	    return "Sesión cerrada correctamente";
+	}
+	
+	@GetMapping("/perfil")
+	public Object sesion(HttpSession session) {
+
+	    Usuarios usuario = (Usuarios) session.getAttribute("usuario");
+
+	    if (usuario == null) {
+	        return "No hay ninguna sesion";
+	    }
+
+	    return usuario;
+	}
+
 }

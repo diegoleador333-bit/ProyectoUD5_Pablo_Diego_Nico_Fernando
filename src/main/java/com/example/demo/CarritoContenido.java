6e0716b065b928918d9183fe3eb04ea370b26b21
Fernando García <garcia.fernandomg@gmail.com>
2026-01-18 14:06:46 +0100

    Commit Endpoints Carrito

--- src/main/java/com/example/demo/CarritoContenido.java
+++ src/main/java/com/example/demo/CarritoContenido.java
@@ -1,49 +1,49 @@
-package com.example.demo;
-
-public class CarritoContenido {
-	private int id;
-	private Carrito carrito;
-	private Camisetas camiseta;
-	private int cantidad;
-
-	public CarritoContenido(int id, Carrito carrito, Camisetas camiseta, int cantidad) {
-		this.id = id;
-		this.carrito = carrito;
-		this.camiseta = camiseta;
-		this.cantidad = cantidad;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public Carrito getCarrito() {
-		return carrito;
-	}
-
-	public void setCarrito(Carrito carrito) {
-		this.carrito = carrito;
-	}
-
-	public Camisetas getCamiseta() {
-		return camiseta;
-	}
-
-	public void setCamiseta(Camisetas camiseta) {
-		this.camiseta = camiseta;
-	}
-
-	public int getCantidad() {
-		return cantidad;
-	}
-
-	public void setCantidad(int cantidad) {
-		this.cantidad = cantidad;
-	}
-
+	package com.example.demo;
 	
-}
+	public class CarritoContenido {
+		private int id;
+		private Carrito carrito;
+		private Camisetas camiseta;
+		private int cantidad;
+	
+		public CarritoContenido(int id, Carrito carrito, Camisetas camiseta, int cantidad) {
+			this.id = id;
+			this.carrito = carrito;
+			this.camiseta = camiseta;
+			this.cantidad = cantidad;
+		}
+	
+		public int getId() {
+			return id;
+		}
+	
+		public void setId(int id) {
+			this.id = id;
+		}
+	
+		public Carrito getCarrito() {
+			return carrito;
+		}
+	
+		public void setCarrito(Carrito carrito) {
+			this.carrito = carrito;
+		}
+	
+		public Camisetas getCamiseta() {
+			return camiseta;
+		}
+	
+		public void setCamiseta(Camisetas camiseta) {
+			this.camiseta = camiseta;
+		}
+	
+		public int getCantidad() {
+			return cantidad;
+		}
+	
+		public void setCantidad(int cantidad) {
+			this.cantidad = cantidad;
+		}
+	
+		
+	}
